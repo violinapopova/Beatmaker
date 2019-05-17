b38eacca2a401818d8067ce3073e674b9d3a0bcf
violinapopova <violipopova@gmail.com>
2019-05-18 02:29:06 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#9bc01c",
+    "#f47d4a",
+    "#e1315b",
+    "#ffec5c",
+    "#008dcb",
+    "#ce83dd"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
